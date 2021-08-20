05484b04f8410f5b2fa8351e83a4856da4211c46
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2021-08-20 19:41:41 +0300

    Moving player's sprite

--- TopDownDungeon/Assets/Scripts/Player.cs
+++ TopDownDungeon/Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();    
+
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        //get input from user
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //swap sprite direction, wether you move left or right
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one; //player sprite swap to the right
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);   //player sprite swap to the left
+
+        //make player move
+        transform.Translate(moveDelta * Time.deltaTime);    //player can move left, right, up, down with this code
+
+        
+
         
     }
 }
