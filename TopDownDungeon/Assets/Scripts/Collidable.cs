c5a45f7542da20ecfadec25b16d640ac56a1ce31
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2021-11-28 21:37:29 +0200

    Collidable Objects

--- TopDownDungeon/Assets/Scripts/Collidable.cs
+++ TopDownDungeon/Assets/Scripts/Collidable.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))] // automatically add a collider 2D to your object
 public class Collidable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ContactFilter2D filter2D;
+    private BoxCollider2D boxCollider;
+    private Collider2D[] hits = new Collider2D[10]; // contain the data of what exactly did you hit frame
+    
+    protected virtual void Start() 
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>(); // whatever object collidable script is on, it requires collider 2D
+
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update() 
     {
-        
+        // Collision works
+        boxCollider.OverlapCollider(filter2D, hits);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null)
+            {
+                continue;
+            }
+
+            OnCollide(hits[i]);
+
+            // the array is not cleaned up, so we do it ourself
+            hits[i] = null;
+        }
     }
+
+    //using inheritence
+    protected virtual void OnCollide(Collider2D coll)
+    {
+        Debug.Log(coll.name);
+    }
+
 }
