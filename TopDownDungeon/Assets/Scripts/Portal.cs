2be9e2992c0fdd936048ce30f241f56227d234cb
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2022-02-24 20:24:19 +0200

    Portal Object

---
+++ TopDownDungeon/Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : Collidable
+{
+    public string[] scenesNames;
+
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+        {
+            // Tele port the player
+
+            // this line create a new scene by itself and it gonna pick between 0 and the length of the scene array
+            string scenesName = scenesNames[Random.Range(0, scenesNames.Length)];
+
+            // load scene
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scenesName);
+        }
+    }
+}
